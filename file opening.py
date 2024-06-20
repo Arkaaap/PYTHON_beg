2b0cff44374f239467fa13ba5d6064fedf3aa65d
Arka@p <99315231+Arkaaap@users.noreply.github.com>
2024-06-20 11:10:22 +0530

    fileopening.py date 6/20/2024

---
+++ file opening.py
@@ -0,0 +1,10 @@
+file = open ("Basic.txt",'a')
+file1  = open("Basic2.txt",'w')
+file.write  ("My 1st python inbuilt file ")
+file.write ("Hope y'all like this ")
+file.write("This feels really good")
+file1.write ("Module is working !!")
+file1.read()
+file1.close()
+file.close()
+
